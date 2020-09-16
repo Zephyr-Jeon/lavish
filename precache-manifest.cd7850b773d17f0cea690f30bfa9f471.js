7ae9dccdf3110812502a6a65d767cbac99bef926
Zephyr Jeon <rltjd1258@gmail.com>
2020-09-16 16:43:57 +0900

    Updates

---
+++ precache-manifest.cd7850b773d17f0cea690f30bfa9f471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d406f51cddb6e1de58570e0bf5d82e34",
+    "url": "/lavish/index.html"
+  },
+  {
+    "revision": "47aad71a29c03d49bef8",
+    "url": "/lavish/static/css/main.c1b09de1.chunk.css"
+  },
+  {
+    "revision": "36f76e31b43da4a90e99",
+    "url": "/lavish/static/js/2.1959d1b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lavish/static/js/2.1959d1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aad71a29c03d49bef8",
+    "url": "/lavish/static/js/main.d5ec7325.chunk.js"
+  },
+  {
+    "revision": "0415b07875e463ea89c7",
+    "url": "/lavish/static/js/runtime-main.e14bf541.js"
+  }
+]);
